cd3d5df5444c9d2ad4812f68ec5bdf95cdba2ffe
Spydy-coder <bikashempire321@gmail.com>
2024-02-03 23:48:57 +0530

    change in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-// Import Bootstrap CSS (add this line in your main JavaScript or SCSS file)
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// If you need Bootstrap JavaScript features (like tooltips, modals, etc.), also add:
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
